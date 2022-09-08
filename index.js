c64635fccc68c45c3282f1f365184c4753c1f6bc
lemuelnjuki <lemuel.njuki@student.moringaschool.com>
2022-09-08 10:18:24 +0300

    API addition

---
+++ index.js
@@ -0,0 +1,9 @@
+//defining the method and headers..Api from rapidapi
+const options = {
+    method: 'GET',
+    url: 'https://flixbus.p.rapidapi.com/v1/cities',
+    headers: {
+      'X-RapidAPI-Key': 'ce21454c39msh57903dab40efab4p1414a6jsn9b7602e0363c',
+      'X-RapidAPI-Host': 'flixbus.p.rapidapi.com'
+    }
+  };
